8d5b1793d0ead6a27af4f2a14775198da06b0b2f
emotytto00 <johannes.liikanen@metropolia.fi>
2024-08-29 13:16:29 +0300

    Started to add register logic with UI

--- src/main/java/ofosFrontend/controller/LoginController.java
+++ src/main/java/ofosFrontend/controller/LoginController.java
@@ -2,15 +2,20 @@ package ofosFrontend.controller;
 
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-
+import javafx.stage.Stage;
 import java.io.IOException;
 
 public class LoginController {
     @FXML
-    private Button button;
+    private Button signUpButton;
+    @FXML
+    private Button registerButton;
     @FXML
     private TextField username;
     @FXML
@@ -30,8 +35,39 @@ public class LoginController {
         }
     }
 
+    public void initialize() {
+        signUpButton.setOnAction(event -> {
+            try {
+                goToRegister(event);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
     @FXML
     private void goToRegister(ActionEvent event) throws IOException {
-        System.out.println("Going to register page.");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ofosFrontend/registerUI.fxml"));
+        Parent root = loader.load();
+
+        Stage currentStage = (Stage) signUpButton.getScene().getWindow();
+
+        Scene registerScene = new Scene(root, 600, 400);
+
+        currentStage.setTitle("OFOS Register");
+
+        currentStage.setScene(registerScene);
+
+        currentStage.show();
+    }
+
+    @FXML
+    public void registerUser(ActionEvent event) throws IOException{
+        createUser();
+    }
+
+    @FXML
+    private void createUser() throws IOException {
+        System.out.println("User created successfully.");
     }
 }
