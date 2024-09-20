aac9b23429c2881bfa9c0e6013ce4e07e57b51a4
mikktur <mikko.silenceri@gmail.com>
2024-09-20 11:40:29 +0300

    Started doing shoppingcart functionality

--- src/main/java/ofosFrontend/controller/User/RMenuController.java
+++ src/main/java/ofosFrontend/controller/User/RMenuController.java
@@ -2,6 +2,7 @@ package ofosFrontend.controller.User;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
@@ -11,7 +12,9 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import ofosFrontend.model.Product;
 import ofosFrontend.model.Restaurant;
+import ofosFrontend.model.ShoppingCart;
 import ofosFrontend.service.ProductService;
+import ofosFrontend.session.SessionManager;
 
 import java.io.IOException;
 import java.util.List;
@@ -44,6 +47,7 @@ public class RMenuController {
                 FXMLLoader cardLoader = new FXMLLoader(getClass().getResource("/ofosFrontend/menuItem.fxml"));
                 VBox card = cardLoader.load();
                 VBox cText = (VBox) card.lookup("#itemInfo");
+                Button addToCartButton = (Button) card.lookup("#addToCartButton");
                 ImageView imageView = (ImageView) card.lookup("#itemImage");
                 Label descriptionLabel = (Label) cText.lookup("#itemDesc");
                 Label priceLabel = (Label) cText.lookup("#itemPrice");
@@ -51,6 +55,9 @@ public class RMenuController {
                 nameLabel.setText(product.getProductName());
                 descriptionLabel.setText(product.getProductDesc());
                 priceLabel.setText(String.valueOf(product.getProductPrice()));
+                addToCartButton.setOnMouseClicked(event -> {
+                    addProductToCart(product);
+                });
                 imageView.setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/hamburga.jpg"))));//+ product.getPicture()))
                 menuContainer.getChildren().add(card);
 
@@ -68,6 +75,11 @@ public class RMenuController {
         for(Product product : products) {
             System.out.println("Product: " + product.getProductName());
         }
+    }
+    private void addProductToCart(Product product) {
+        SessionManager sessionManager = SessionManager.getInstance();
+        ShoppingCart cart = sessionManager.getCart();
+        cart.addItem(product, 1);
     }
 
     public void setRestaurant(Restaurant restaurant) {
