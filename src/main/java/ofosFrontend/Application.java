f34de786eaafaa9a64ab912ac19e85a8eddf0f5b
emotytto00 <johannes.liikanen@metropolia.fi>
2024-10-07 18:17:57 +0300

    login screen set to not resizable

--- src/main/java/ofosFrontend/Application.java
+++ src/main/java/ofosFrontend/Application.java
@@ -14,6 +14,7 @@ public class Application extends javafx.application.Application {
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("OFOS Login");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
